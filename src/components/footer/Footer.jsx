feaa5ad5eea608c452ff5b8c66c4fa106c282e44
guacirita <guacirita@gmail.com>
2022-08-18 11:57:02 -0300

    colocando email no footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,7 +2,10 @@ import './footer.styles.css'
 
 function Footer(){
     return(
+        <>
         <p className="footer">Feito com carinho por Jacira</p>
+        <p>meu e-mail é guacirita@gmail.com</p>
+        </>
     )
 }
     export default Footer
