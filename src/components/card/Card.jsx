59a835f8f97825dff7a7fd398fc6fdd5a4375750
guacirita <guacirita@gmail.com>
2022-08-17 23:09:40 -0300

    subindo alterações

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,11 +1,10 @@
-
 import image from "../../assets/eu.jpg"
 import "./card.styles.css"
 
 function Card(){
     return(
         <div className="card-container">
-            <img src={image} alt="Foto de Jacira"></img>
+            <img src={image} alt="Foto de Jacira" />
         </div>
     )
 }
